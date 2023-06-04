cefa99d28eab448b0e33a543abf78ca47b9e629e
nick <nick@pop-os.localdomain>
2023-06-03 22:23:43 -0400

    vite config for apiurl

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    replace({
+      'process.env.API_URL': JSON.stringify(process.env.API_URL || 'http://localhost:5003'),
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
